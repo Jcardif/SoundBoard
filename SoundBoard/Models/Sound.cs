bdf055eafa56576fbe8fe308fe0a3f4c430f4a1a
Jcardif <josh.cardif14@outlook.com>
2018-06-04 12:28:25 +0300

    Add sound Model

---
+++ SoundBoard/Models/Sound.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundBoard.Models
+{
+    public class Sound
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string AudoFile { get; set; }
+        public String ImageFile { get; set; }
+    }
+}
